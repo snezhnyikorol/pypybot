613d9577cd3100458775b14b8f508be15dbe52fd
snezhyikorol <raketskij@gmail.com>
2018-06-21 10:28:50 +0300

    'v'

--- bot.py
+++ bot.py
@@ -3,8 +3,12 @@ import telebot
 from SQLighter import SQLighter
 import random
 import utils
+import os
+from flask import Flask, request
 
 bot = telebot.TeleBot(config.TOKEN)
+
+server = Flask(__name__)
 
 @bot.message_handler(commands=['game'])
 def game(message):
@@ -28,7 +32,16 @@ def check_answer(message):
             bot.send_message(message.chat.id, 'Увы, Вы не угадали. Попробуйте ещё раз!', reply_markup=keyboard_hider)
         utils.finish_user_game(message.chat.id)
 
-if __name__ == '__main__':
-    utils.count_rows()
-    random.seed()
-    bot.polling(none_stop=True)
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://safe-oasis-17008.herokuapp.com/' + TOKEN)
+    return "!", 200
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
